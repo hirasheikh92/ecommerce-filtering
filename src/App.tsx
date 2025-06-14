08a83464970a864f6a6dc115c2a30d936542b027
Hira Moueen <56905693+hirasheikh92@users.noreply.github.com>
2025-06-14 13:37:05 +0500

    add loding spinner

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import { Suspense } from "react";
 import { lazy } from "react";
+import Loading from "./components/Loading";
 
 const Layout = lazy(() => import("./layout/Layout"));
 const MainContent = lazy(() => import("./components/MainContent"));
@@ -9,7 +10,7 @@ const ProductPage = lazy(() => import("./components/ProductPage"));
 function App() {
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loading />}>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<MainContent />} />
